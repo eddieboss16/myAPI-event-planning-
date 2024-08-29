c2c3f1fecdbf515ed2f92a3ab5d278c27f522720
edu <edwinnjoroge001@gmail.com>
2024-08-29 17:30:32 +0300

    added a welcome route and working in the browser

--- server.js
+++ server.js
@@ -6,9 +6,14 @@ const app = express();
 require("dotenv-flow").config();
 
 //route
+app.get("/api/welcome", (req, res)=> {
+    res.status(200).send({message: "welcome to my myAPI"});
+})
+
 
 const PORT = process.env.PORT || 4000;
 
+//start up server
 app.listen(PORT, function(){
     console.log("Server is running on port: " + PORT);
 })
